d81d0db85bd666526f224c6e1ffa331245e55728
user1 <harry.boisselot@edu.univ-fcomte.fr>
2024-11-04 11:13:22 +0100

    Retrouver les modif

--- src/datasource/data.js
+++ src/datasource/data.js
@@ -1,7 +1,13 @@
 let utilisateurs = [
     {"id":1, "prenom":"John", "nom":"Doe", "dateNaissance":"23-09-1998", "email":"johndoe@gmail.com", "motDePasse":"mdp123"},
 ];
+let artistes = [
+    {"id":1, "nomGroupe":"PINK", "nbMembres":5, "decision":"null"},
+    {"id":2, "nomGroupe":"KISS", "nbMembres":4, "decision":"null"},
+    {"id":3, "nomGroupe":"BABY", "nbMembres":2, "decision":"null"},
+];
 
 module.exports = {
     utilisateurs,
+    artistes
 }
