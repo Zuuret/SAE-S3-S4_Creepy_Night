5c1a685d339378dee9d9fffe14ae9ce1f42035f5
user1 <harry.boisselot@edu.univ-fcomte.fr>
2024-11-04 11:23:55 +0100

    Création de la page visuelle

---
+++ src/views/OrgaValidArtisteView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <h1>Validation de concert</h1>
+    <p>Liste des artistes</p>
+    <ul class="validation-table">
+      <li v-for="(artiste) in artistes" :key="artiste.id">
+        <div v-if="artiste.decision == 'null'">
+          {{ artiste.nomGroupe }}, {{ artiste.nbMembres }}
+          <button @click="setDecision(['true', artiste.id])"> Accepter </button>
+          <button @click="setDecision(['false', artiste.id])"> Refuser </button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  name: 'AboutView.vue',
+  data: () => ({
+  }),
+  computed: {
+    ...mapState(['artistes']),
+  },
+  methods: {
+    ...mapActions(['setDecision','getArtistes']),
+  },
+  mounted() {
+    this.getAllUser();
+  }
+}
+
+</script>
+
+<style>
+
+</style>
