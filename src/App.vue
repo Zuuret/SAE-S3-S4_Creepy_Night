5b75519fbd395931e1a12e8e126a178dc6a35a9c
Tim Carrara <carrara.tim85@gmail.com>
2024-11-11 16:47:59 +0100

    Reconfiguration Accueil

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,15 @@
 <template>
-  <div id="app">
-    <nav>
-      <CompteRebours />
-      <router-link to="/profil">Créer profil</router-link>
-      <p></p>
-      <router-link to="/concert">Réserver un concert</router-link>
-      <br><br>
-      <router-link to="/concert-schedule">Programmation des concerts</router-link>
-      <br><br>
-      <router-link to="/planning">Placer concert</router-link>
-      <br><br>
-      <router-link to="/organisateur/validartiste">Valider concert</router-link>
-    </nav>
+  <div class="app">
     <router-view/>
   </div>
-
 </template>
 
 <script>
-import CompteRebours from './components/CompteRebours.vue';
 
 export default {
-  components: {
-    CompteRebours,
-  },
+  name: 'App',
+
+  data: () => ({
+  }),
 };
 </script>
-
-<style>
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
