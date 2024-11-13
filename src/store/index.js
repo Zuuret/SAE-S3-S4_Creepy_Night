00063d36f05c43cea0cb65ef59cf7483a9550555
user1 <harry.boisselot@edu.univ-fcomte.fr>
2024-11-13 10:55:26 +0100

    suppression de fichiers superflux

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import controller from "@/datasource/controller";
 
 Vue.use(Vuex)
 
-import ProfilService from '../services/profil.service'
 import ValidArtiste from '../services/validArtiste.service'
 
 export default new Vuex.Store({
   state: {
-    utilisateur: null,
-    utilisateurs: [],
     artistes: [],
     artiste: [],
   },
   getters: {
   },
   mutations: {
-    updateUtilisateur(state, utilisateur){
-      state.utilisateur = utilisateur;
-    },
-    updateListeUtilisateur(state, utilisateurs){
-      state.utilisateurs = utilisateurs;
-    },
     updateListeArtistes(state, artistes){
       state.artistes = artistes;
     },
@@ -31,26 +21,6 @@ export default new Vuex.Store({
     },
   },
   actions: {
-    async enregistrementUtilisateur({commit}, data){
-      console.log("enregistrement d'un nouvel utilisateur")
-      let response = await controller.ajoutUtilisateur(data);
-      if(response.error === 0){
-        commit('updateUtilisateur', response.data);
-        return true;
-      } else {
-        console.log(response.data);
-        return false;
-      }
-    },
-    async getAllUser({ commit }) {
-      console.log("Récupération des utilisateurs");
-      let response = await ProfilService.getAllUsers();
-      if (response.error === 0) {
-        commit('updateListeUtilisateur', response.data);
-      } else {
-        console.log(response.data);
-      }
-    },
     async getArtistes({ commit }) {
       console.log("Récupération des artistes");
       let response = await ValidArtiste.getArtistes();
