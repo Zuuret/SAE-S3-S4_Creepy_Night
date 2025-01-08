575d9fda81dffc32ce83a44ceed84b439ce5cc53
EnzoVidy <enzo.vidy1@gmail.com>
2025-01-08 16:44:38 +0100

    Middleware de session corrigé (ne renvoiee plus tout le temps une erreur)

--- serveur/middlewares/session.middleware.js
+++ serveur/middlewares/session.middleware.js
@@ -1,10 +1,21 @@
 exports.checkSession = (req, res, next) => {
+
+    // Supposition que le front-end envoie l'identifiant de session
+    // obtenu lors de l'authentification, via la partie query de l'URL
+    // Code à supprimer une fois le front-end en place et qui envoie l'identifiant de session
+    if (!req.query.session) {
+        req.query.session = "12abc45-953-cfb12";
+    }
+    // Fin du code à supprimer
+
+
     const session = req.query.session;
     if (!session) {
         return res.status(401).json({ error: "Session manquante ou invalide." });
     }
 
-    // vérifications supplémentaires (ex : validation de l'identifiant de session en base de données / check role session).
+    // vérifications supplémentaires (ex : validation de l'identifiant de
+    // session en base de données / check role session).
     // ...
 
     console.log(`Session valide : ${session}`);
