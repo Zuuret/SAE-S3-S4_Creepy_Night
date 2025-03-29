034e94f33cb39bb4bd44f2b7d98a612a285233e5
user1 <harry.boisselot@edu.univ-fcomte.fr>
2025-03-29 13:19:01 +0100

    save middleware de session non fonctionnel pour le moment

--- serveur/middlewares/session.middleware.js
+++ serveur/middlewares/session.middleware.js
@@ -1,4 +1,8 @@
-exports.checkSession = (req, res, next) => {
+const User = require('../controllers/users.controller');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+module.exports.checkSession = (req, res, next) => {
 
     // Supposition que le front-end envoie l'identifiant de session
     // obtenu lors de l'authentification, via la partie query de l'URL
@@ -20,4 +24,29 @@ exports.checkSession = (req, res, next) => {
 
     console.log(`Session valide : ${session}`);
     next();
+};
+
+module.exports.signin = (req, res) => {
+    User.getUserById(req, res)
+        .then(user => {
+            let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+            if (!passwordIsValid) {
+                return res.status(401).send({
+                    accessToken: null,
+                    error: "Mot de passe incorrect!"
+                });
+            }
+            let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+                // expiresIn: 86400 // 24 heures
+                expiresIn: 600*2 // 20 minutes
+            });
+            res.status(200).send({
+                id: user.id,
+                username: user.email,
+                accessToken: token
+            });
+        })
+        .catch(err => {
+            res.status(500).send({ error: err.message });
+        });
 };
