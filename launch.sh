300b321a79731c94cdf9ea92799fad2e8927e92b
Zuuret <yahia.kherza@proton.me>
2025-03-31 16:59:20 +0200

    launcher windows compatible

--- launch.sh
+++ launch.sh
@@ -2,19 +2,28 @@
 
 # usage : ./launch.sh [cible] | cible : "serveur" pour lancer le serveur seul | rien pour lancer le serveur et le client
 
+OS=$(uname -s)
+run_npm() {
+    cd "$1" || exit 1
+    npm install
+    if [ "$OS" = "MINGW64_NT-10.0" ] || [ "$OS" = "MSYS_NT-10.0" ]; then
+        start cmd.exe /c "npm start"
+    else
+        npm start &
+        echo $!
+    fi
+}
+
 git pull
 
 if [ "$1" = "serveur" ]; then
-    cd serveur/ || exit
-    npm install
-    npm start
+    run_npm "serveur"
 else
-    cd serveur/ || exit
-    npm install
-    npm start &
-    SERVER_PID=$!
-    cd ../client/ || exit
+    SERVER_PID=$(run_npm "serveur")
+    cd ../client/ || exit 1
     npm install
     npm run serve
-    wait $SERVER_PID
+    if [ "$OS" != "MINGW64_NT-10.0" ] && [ "$OS" != "MSYS_NT-10.0" ]; then
+        wait "$SERVER_PID"
+    fi
 fi
