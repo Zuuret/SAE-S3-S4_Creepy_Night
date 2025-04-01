758a591c6c2b90de06a8c9ac12c144becf1dc4ab
user1 <harry.boisselot@edu.univ-fcomte.fr>
2025-04-01 16:38:16 +0200

    Mise en place front-back pour les articles Prestataire

--- serveur/controllers/articles.controller.js
+++ serveur/controllers/articles.controller.js
@@ -13,3 +13,57 @@ exports.getAllArticlesById = async (req, res) => {
         return res.status(500).json({ error: "Erreur interne du serveur" });
     }
 };
+
+exports.getAllArticles = async (req, res) => {
+    const articles = await articlesService.getAllArticles();
+    if (!articles) {
+        return res.status(500).json({ error: 'ERREUR INTERNE' });
+    }
+    return res.status(200).json({ data: articles });
+};
+
+exports.saveArticle = async (req,res) => {
+    const prestataire_id = req.body.prestataire_id;
+    const nom = req.body.nom;
+    const description = req.body.description;
+    const prix = req.body.prix;
+    const stock = req.body.stock;
+    const image = req.body.image;
+    const resultat = await articlesService.insertArticle(prestataire_id, nom, description, prix, stock, image);
+    if (resultat) {
+        return res.status(500).send("ERREUR INTERNE");
+    }
+    return res.status(200).send("INSERTION AVEC SUCCES");
+};
+
+exports.updateArticle = async (req,res) => {
+    const id = req.params.id;
+    const prestataire_id = req.body.prestataire_id;
+    const nom = req.body.nom;
+    const description = req.body.description;
+    const prix = req.body.prix;
+    const stock = req.body.stock;
+    const image = req.body.image;
+    const resultat = await articlesService.updateArticle(id, prestataire_id, nom, description, prix, stock, image);
+    if(resultat){
+        return res.status(500).send("ERREUR INTERNE");
+    }
+    return res.status(200).send("MODIFICATION ENREGISTREE");
+};
+
+exports.deleteArticle = async (req, res) => {
+    const { id } = req.params;
+    //console.log(id)
+    try {
+        const result = await articlesService.deleteArticle(id);
+
+        if (!result) {
+            return res.status(200).json({ message: 'Article supprimé avec succès' });
+        } else {
+            return res.status(404).json({ message: 'Article non trouvé' });
+        }
+    } catch (error) {
+        console.error("Erreur lors de la suppression d'article:", error);
+        return res.status(500).json({ message: 'Erreur interne du serveur' });
+    }
+};
--- serveur/services/articles.service.pg.js
+++ serveur/services/articles.service.pg.js
@@ -1,4 +1,5 @@
 const pool = require('../database/db');
+const format = require('pg-format');
 
 async function getAllArticlesById(idPrestataire) {
     const client = await pool.connect();
@@ -14,6 +15,77 @@ async function getAllArticlesById(idPrestataire) {
     }
 }
 
+async function getAllArticles() {
+    const client = await pool.connect();
+    let res;
+    try {
+        res = await client.query('SELECT * FROM articles');
+        console.log('RECUPERATION DES ARTICLES');
+    } catch (error) {
+        console.error('Erreur lors de la récupération des articles :', error);
+        res = false;
+    } finally {
+        client.release();
+    }
+    return res.rows || false;
+}
+
+async function insertArticle(prestataire_id, nom, description, prix, stock, image) {
+    const client = await pool.connect();
+    let is_error = false;
+    try {
+        const data = [
+            [prestataire_id, nom, description, prix, stock, image]
+        ]
+        const query = format('INSERT INTO articles (prestataire_id, nom, description, prix, stock, image) VALUES %L', data);
+        await client.query(query);
+        console.log('INSERTIONS AVEC SUCCES');
+    } catch (error) {
+        console.error('Erreur : ', error);
+        is_error = true;
+    } finally {
+        client.release();
+    }
+    return is_error;
+}
+
+async function updateArticle(id, prestataire_id, nom, description, prix, stock, image) {
+    const client = await pool.connect();
+    let is_error = false;
+    try {
+        const query = format('UPDATE articles SET prestataire_id = %L, nom = %L, description = %L, prix = %L, stock = %L, image = %L WHERE id = %L', prestataire_id, nom, description, prix, stock, image, id);
+        await client.query(query);
+        console.log('MISE A JOUR DU CONCERT');
+    } catch (error) {
+        console.error('Erreur lors de la mise à jour du concert :', error);
+        is_error = true;
+    } finally {
+        client.release();
+    }
+    return is_error;
+}
+
+async function deleteArticle(id) {
+    const client = await pool.connect();
+    let is_error = false;
+    console.log(id)
+    try {
+        const query = format('DELETE FROM articles WHERE id = %L RETURNING *',id);
+        await client.query(query);
+        console.log('SUPPRESSION ARTICLE');
+    } catch (error) {
+        console.error("Erreur lors de la suppression d'article :", error);
+        is_error = true;
+    } finally {
+        client.release();
+    }
+    return is_error;
+}
+
 module.exports = {
     getAllArticlesById,
+    getAllArticles,
+    insertArticle,
+    updateArticle,
+    deleteArticle,
 };
