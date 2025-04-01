b9ed5656af7cf3aa59ef101349f5e471ed0655e0
EnzoVidy <enzo.vidy1@gmail.com>
2025-04-01 10:58:07 +0200

    Correction bug page prestataire côté public

--- serveur/controllers/articles.controller.js
+++ serveur/controllers/articles.controller.js
@@ -5,9 +5,9 @@ exports.getAllArticlesById = async (req, res) => {
     try {
         const articles = await articlesService.getAllArticlesById(idPrestataire);
         if (!articles || articles.length === 0) {
-            return res.status(404).json({ error: "Aucun article trouvé pour ce prestataire" });
+            return res.status(200).json({ data: [], error: 0 });
         }
-        return res.status(200).json({data: articles, error: 0});
+        return res.status(200).json({ data: articles, error: 0 });
     } catch (error) {
         console.error("Erreur dans le contrôleur getAllArticlesById :", error);
         return res.status(500).json({ error: "Erreur interne du serveur" });
