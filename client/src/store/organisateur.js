248e622b3b4f8fdb2a63f0e2ebce20dc1536a38e
matheorose <matheorose1703@gmail.com>
2025-04-01 14:33:30 +0200

    graphique côté orga

--- client/src/store/organisateur.js
+++ client/src/store/organisateur.js
@@ -1,13 +1,17 @@
-import { getBilletsAchetesAujourdHui } from "@/services/organisateur.service";
+import { getBilletsAchetesAujourdHui, getBilletsAchetesCetteSemaine } from "@/services/organisateur.service";
 
 const state = {
     billetsAchatAujourdHui: 0,
+    historiqueVentes: [] // Ajout pour stocker les données historiques
 };
 
 const mutations = {
     SET_BILLETS_AUJOURD_HUI(state, billets) {
-        state.billetsAchatAujourdHui = billets.length; // si billets est un tableau
+        state.billetsAchatAujourdHui = billets.length;
     },
+    SET_HISTORIQUE_VENTES(state, historique) {
+        state.historiqueVentes = historique;
+    }
 };
 
 const actions = {
@@ -17,10 +21,17 @@ const actions = {
             commit('SET_BILLETS_AUJOURD_HUI', response.data);
         }
     },
+    async fetchHistoriqueVentes({ commit }) {
+        const response = await getBilletsAchetesCetteSemaine();
+        if (response.error === 0) {
+            commit('SET_HISTORIQUE_VENTES', response.data);
+        }
+    }
 };
 
 const getters = {
     billetsAchatAujourdHui: (state) => state.billetsAchatAujourdHui,
+    historiqueVentes: (state) => state.historiqueVentes
 };
 
 export default {
